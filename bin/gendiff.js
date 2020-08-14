37b4467715c930afd333c2e8ad030c8e3bea4a4e
Dmitriy Balashov <mockedcat@gmail.com>
2020-08-14 22:09:30 +0300

    basic recursive json gendiff implemented

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new commander.Command();
 
@@ -8,6 +9,10 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(JSON.stringify(diff, null, '\t').replace(/["|,]/g, ''));
+  })
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format');
 
---
+++ src/index.js
@@ -0,0 +1,105 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const {
+  has, union, sortBy, isPlainObject,
+} = _;
+
+function getDiff(obj1, obj2) {
+  const allKeys = union(Object.keys(obj1).concat(Object.keys(obj2)));
+
+  const diff = allKeys.reduce((acc, key) => {
+    // удаление
+    if (has(obj1, key) && !has(obj2, key)) {
+      return [...acc, {
+        key,
+        action: 'deleted',
+        prevValue: obj1[key],
+      }];
+    }
+
+    // добавление
+    if (!has(obj1, key) && has(obj2, key)) {
+      return [...acc, {
+        key,
+        action: 'added',
+        value: obj2[key],
+      }];
+    }
+
+    // изменение
+    if (has(obj1, key) && has(obj2, key)) {
+      if (obj1[key] === obj2[key]) {
+        return [...acc, {
+          key,
+          action: 'not_edited',
+          value: obj2[key],
+        }];
+      }
+
+      // TODO: добавить обработку массивов
+      if (isPlainObject(obj1[key]) && isPlainObject(obj2[key])) {
+        return [...acc, {
+          key,
+          action: 'edited',
+          diff: getDiff(obj1[key], obj2[key]),
+        }];
+      }
+
+      return [...acc, {
+        key,
+        action: 'deleted',
+        prevValue: obj1[key],
+      }, {
+        key,
+        action: 'added',
+        value: obj2[key],
+      }];
+    }
+
+    return acc;
+  }, []);
+
+  return sortBy(diff, ['key']);
+}
+
+function prettyDiff(diff) {
+  return diff.reduce((acc, item) => {
+    const { key, action } = item;
+    if (action === 'deleted') {
+      return {
+        ...acc,
+        [`- ${key}`]: item.prevValue,
+      };
+    }
+
+    if (action === 'added') {
+      return {
+        ...acc,
+        [`+ ${key}`]: item.value,
+      };
+    }
+
+    if (item.diff) {
+      return {
+        ...acc,
+        [key]: prettyDiff(item.diff),
+      };
+    }
+
+    return {
+      ...acc,
+      [`  ${key}`]: item.value,
+    };
+  }, {});
+}
+
+function genDiff(filepath1, filepath2) {
+  const filedata1 = JSON.parse(readFileSync(path.resolve(filepath1)));
+  const filedata2 = JSON.parse(readFileSync(path.resolve(filepath2)));
+
+  return prettyDiff(getDiff(filedata1, filedata2));
+}
+
+export default genDiff;
