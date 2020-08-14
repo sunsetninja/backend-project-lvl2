e20bbc25b760a2de5f52f58722a46db135fbdfb0
Dmitriy Balashov <mockedcat@gmail.com>
2020-08-14 20:58:05 +0300

    format cli option description added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,8 @@ const program = new commander.Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
