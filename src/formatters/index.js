d64c6910503ad7ca7abcade33437490dec76dcbc
Dmitriy Balashov <mockedcat@gmail.com>
2020-08-26 12:31:24 +0300

    json formatter added

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,11 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const formatters = {
   stylish: stylish.format,
   plain: plain.format,
+  json: json.format,
 };
 
 function formatDiff(diff, type) {
---
+++ src/formatters/json.js
@@ -0,0 +1,3 @@
+const format = JSON.stringify;
+
+export default { format };
